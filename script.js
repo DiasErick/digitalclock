95260c43e3c5540af664c0f112263cc84faa2d7e
DiasErick <116904851+DiasErick@users.noreply.github.com>
2022-10-29 21:55:20 -0300

    Adding date details

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function getTime(){
     var dateTime = new Date();    
     var hours = dateTime.getHours().toString().padStart(2, "0");
     var minutes = dateTime.getMinutes().toString().padStart(2, "0");
-    var seconds = dateTime.getSeconds().toString().padStart(2, "0");    
+    var seconds = dateTime.getSeconds().toString().padStart(2, "0");
+    var day = dateTime.getDate();    
+    var month = dateTime.toLocaleString('default', { month: 'long' });
+    var weekday = dateTime.toLocaleString('default', { weekday: 'long' });
+    var year = dateTime.getFullYear();            
+    var date = weekday + ", " + month + " " + day + ", " +  year;
     var ampm = 'AM'
     if (hours >= 12){
         ampm = "PM"
@@ -13,6 +18,7 @@ function getTime(){
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
     document.getElementById('ampm').innerHTML = ampm;
+    document.getElementById('date').innerHTML = date;   
 }
 
 setInterval(getTime,10)
